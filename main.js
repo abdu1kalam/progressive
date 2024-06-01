b3fb8936bffdc30efc9f7199f40b4bc55f3e5062
abdul kalam <babbu36790@gmail.com>
2024-06-01 23:17:52 +0530

    Third

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("service-worker.js").then(function () {
     console.log("Service Worker Registered");
-  });
+  }).catch(function(error) {
+    console.error('Service Worker Registration Failed:', error);
+});
 }
 
 let deferredPrompt;
+const installBtn = document.getElementById('install-btn');
+
 window.addEventListener("beforeinstallprompt", (e) => {
+  console.log('beforeinstallprompt event fired');
   e.preventDefault();
   deferredPrompt = e;
+  installBtn.style.display = 'block';
+
+  installBtn.addEventListener('click', () => {
+    installBtn.style.display = 'none';
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the install prompt');
+        } else {
+            console.log('User dismissed the install prompt');
+        }
+        deferredPrompt = null;
+    });
+});
+
+});
+
+window.addEventListener('appinstalled', (evt) => {
+  console.log('a2hs', 'installed');
 });
 
 const notifyBtn = document.getElementById("notify-btn");
