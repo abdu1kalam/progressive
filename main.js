d3c669b60594b370efed1c37a93470e81229a4a3
abdul kalam <babbu36790@gmail.com>
2024-06-02 06:24:46 +0530

    added code

--- main.js
+++ main.js
@@ -1,49 +1,75 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("service-worker.js").then(function () {
-    console.log("Service Worker Registered");
-  }).catch(function(error) {
-    console.error('Service Worker Registration Failed:', error);
-});
-}
-
-let deferredPrompt;
-const installBtn = document.getElementById('install-btn');
-
-window.addEventListener("beforeinstallprompt", (e) => {
-  console.log('beforeinstallprompt event fired');
-  e.preventDefault();
-  deferredPrompt = e;
-  installBtn.style.display = 'block';
-
-  installBtn.addEventListener('click', () => {
-    installBtn.style.display = 'none';
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the install prompt');
-        } else {
-            console.log('User dismissed the install prompt');
-        }
-        deferredPrompt = null;
-    });
-});
-
-});
-
-window.addEventListener('appinstalled', (evt) => {
-  console.log('a2hs', 'installed');
-});
-
-const notifyBtn = document.getElementById("notify-btn");
-notifyBtn.addEventListener("click", () => {
-  Notification.requestPermission().then((permission) => {
-    if (permission === "granted") {
-      navigator.serviceWorker.getRegistration().then((reg) => {
-        reg.showNotification("Hello!", {
-          body: "This is a push notification.",
-          icon: "icon.svg",
+const APP = {
+  deferredInstall: null,
+  init() {
+    if ("serviceWorker" in navigator) {
+      //register our service worker
+      navigator.serviceWorker
+        .register("/service-worker.js", {
+          updateViaCache: "none",
+          scope: "/",
+        })
+        .then(() => {
+          console.log("finished registering");
+        })
+        .catch((err) => {
+          console.warn("Failed to register", err.message);
         });
+      //listen for messages
+      navigator.serviceWorker.addEventListener("message", ({ data }) => {
+        //received a message from the service worker
+        console.log(data, "from service worker");
+      });
+
+      //listen for `appinstalled` event
+      window.addEventListener("appinstalled", (evt) => {
+        //deprecated but still runs in Chrome-based browsers.
+        //Not very useful event.
+        //Better to use the DOMContentLoaded and then look at how it was launched
+      });
+
+      //listen for `beforeinstallprompt` event
+      window.addEventListener("beforeinstallprompt", (ev) => {
+        // Prevent the mini-infobar from appearing on mobile
+        ev.preventDefault();
+        // Stash the event so it can be triggered later.
+        APP.deferredInstall = ev;
+        console.log("saved the install event");
+        // Update UI notify the user they can install the PWA
+        // if you want here...
+      });
+
+      let btn = document.getElementById("btnInstall");
+      btn?.addEventListener("click", APP.startChromeInstall);
+    }
+    
+    const notifyBtn = document.getElementById("notify-btn");
+    notifyBtn.addEventListener("click", () => {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          navigator.serviceWorker.getRegistration().then((reg) => {
+            reg.showNotification("Hello!", {
+              body: "This is a push notification.",
+              icon: "icon.svg",
+            });
+          });
+        }
+      });
+    });
+  },
+  startChromeInstall() {
+    if (APP.deferredInstall) {
+      console.log(APP.deferredInstall);
+      APP.deferredInstall.prompt();
+      APP.deferredInstall.userChoice.then((choice) => {
+        if (choice.outcome == "accepted") {
+          //they installed
+          console.log("installed");
+        } else {
+          console.log("cancel");
+        }
       });
     }
-  });
-});
+  },
+};
+
+document.addEventListener("DOMContentLoaded", APP.init);
--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,61 @@
-self.addEventListener("install", function (event) {
+const version = 6;
+const preCacheName = `static-${version}`;
+const preCache = ["/", "/index.html", "/404.html"];
+
+self.addEventListener("install", function (ev) {
+  ev.waitUntil(
+    caches
+      .open(preCacheName)
+      .then((cache) => {
+        console.log("caching the static files");
+        cache.addAll(preCache);
+      })
+      .catch(console.warn)
+  );
   console.log("Service Worker Installed");
 });
 
-self.addEventListener("activate", function (event) {
+self.addEventListener("activate", function (ev) {
+  ev.waitUntil(
+    caches
+      .keys()
+      .then((keys) => {
+        return Promise.all(
+          keys
+            .filter((key) => key !== preCacheName)
+            .map((key) => caches.delete(key))
+        );
+      })
+      .catch(console.warn)
+  );
   console.log("Service Worker Activated");
 });
 
-self.addEventListener("fetch", function (event) {
-  console.log("Service Worker Fetch", event.request.url);
+self.addEventListener("fetch", function (ev) {
+  console.log("Service Worker Fetch", ev.request.url);
+
+  ev.respondWith(
+    caches.match(ev.request).then((cacheRes) => {
+      return (
+        cacheRes ||
+        fetch(ev.request).then(
+          (response) => {
+            return response;
+          },
+          (err) => {
+            //network failure
+            //send something else from the cache?
+            if (
+              ev.request.url.indexOf(".html") > -1 ||
+              ev.request.mode == "navigation"
+            ) {
+              return caches.match("/404.html");
+            }
+          }
+        )
+      );
+    })
+  );
 });
 
 self.addEventListener("push", function (event) {
@@ -19,3 +67,18 @@ self.addEventListener("push", function (event) {
     self.registration.showNotification("Push Notification", options)
   );
 });
+
+self.addEventListener("message", (ev) => {
+  let data = ev.data;
+  console.log("SW received", data);
+});
+
+const sendMessage = async (msg) => {
+  let allClients = await clients.matchAll({ includeUncontrolled: true });
+  return Promise.all(
+    allClients.map((client) => {
+      let channel = new MessageChannel();
+      return client.postMessage(msg);
+    })
+  );
+};
